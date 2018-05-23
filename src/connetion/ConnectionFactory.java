1d98b0b4f75db487554ca2fd0d7ec7d845b1917f
Gabriel Barreto <gsbarretobr@gmail.com>
2018-05-23 17:23:12 -0300

    Inserido a tela de login

--- src/connetion/ConnectionFactory.java
+++ src/connetion/ConnectionFactory.java
@@ -23,7 +23,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/projetoengsoft";
     private static final String USER = "root";
-    private static final String PASS = "data93";
+    private static final String PASS = "biel1996";
     
     
     public static Connection getConnection(){
--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -9,6 +9,8 @@ import connetion.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.*;
+import java.sql.ResultSet;
 import model.bean.Cliente;
 
 /**
@@ -41,6 +43,77 @@ public class ClienteDAO {
             ConnectionFactory.closeConnection(con, stmt);
         }
     }
+    
+    public boolean update(Cliente cliente){
+        
+        String sql = "update usuario set nome=?,senha=? where email=?";
+        
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getSenha());
+            stmt.setString(3, cliente.getEmail());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.err.print("Erro: "+ex);
+            return false;            
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    
+    public boolean delete(Cliente cliente){
+        
+        String sql = "delete from usuario where email=?";
+        
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, cliente.getEmail());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.err.print("Erro: "+ex);
+            return false;            
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    
+    public List<Cliente> getClientes(){
+        List<Cliente> lista = new ArrayList<Cliente>();
+        ResultSet rs = null;
+        String sql = "select * from usuario";
+        
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Cliente cliente = new Cliente();
+                cliente.setIdCli(Integer.valueOf(rs.getString("idUser")));
+                cliente.setEmail(rs.getString("email"));
+                cliente.setEmail(rs.getString("senha"));
+                cliente.setEmail(rs.getString("nome"));
+                lista.add(cliente);
+            }
+            
+            rs.close();
+            stmt.close();
+            
+            return lista;
+            
+        } catch (SQLException ex) {
+            System.err.print("Erro: "+ex);
+            //Falta adicionar exception
+            return null;
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    } 
     
     
     
